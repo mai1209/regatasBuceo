f2ce96c59721d986f40a2e8b39b517f685cd32de
Mai <maidev@Mac.fibertel.com.ar>
2025-10-01 18:09:58 -0300

    deploytwo

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/buceo/", // <- Cambiá "buceo" por el nombre exacto de tu repo
+  base: "/regatasBuceo/", // <- Cambiá "buceo" por el nombre exacto de tu repo
 })
