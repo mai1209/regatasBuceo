14fd8608fcf533947682b924e9821b8b66c30669
Mai <maidev@MacBook-Air-de-Mai.local>
2025-10-16 10:57:20 -0300

    arreglo de width pagina general

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,11 +2,12 @@
 import style from '../styles/Home.module.css'
 
 function Home() {
-  
+
   return (
     <div className={style.container}>
-      <h2 className={style.title}>EXPLORÁ LAS PROFUNDIDADES <br />
-        CON BUZOS DE REGATAS</h2>
+      <div className={style.containerWidth}>
+        <h2 className={style.title}>EXPLORÁ LAS PROFUNDIDADES <br />
+          CON BUZOS DE REGATAS</h2>
         <p className={style.infoHome}>
           Celebramos 35 años bajo el aguaDesde 1990 acompañamos a buzos de todas las edades y niveles a descubrir el fascinante mundo submarino. Tres décadas formando aventureros, compartiendo experiencias inolvidables y construyendo una comunidad unida por la pasión por el buceo.Gracias por ser parte de esta historia que sigue creciendo día a día
         </p>
@@ -14,6 +15,7 @@ function Home() {
           <button>EXPLORAR CURSOS</button>
           <button className={style.containerBtnDos}>CONTACTANOS AHORA</button>
         </div>
+      </div>
     </div>
   )
 }
